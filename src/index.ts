6dbcc59110c32ed3d8e35a8da432e9f71b7295fc
Tom <thomas.munro@btinternet.com>
2022-07-17 08:54:07 +0100

    :sparkles: send text

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import aws from "aws-sdk";
 import scrapeMagicSeaweed from "./magic-seaweed";
 // import data from "../stubbed-data.json";
 import { getGoodBegginerPaddleboardingWindows } from "./magic-seaweed/paddeboarding";
@@ -5,10 +6,14 @@ import { getGoodSurfWindows } from "./magic-seaweed/surfing";
 import { composeMessage } from "./messages/composeMessage";
 import { groupForecasts } from "./messages/formatMessage";
 
+aws.config.update({ region: "eu-west-2" });
+
 main().catch((err) => {
   console.error(err);
   process.exit(0);
 });
+
+const phoneNumbers = ["+447947614588"];
 
 async function main() {
   const data = await scrapeMagicSeaweed();
@@ -22,7 +27,16 @@ async function main() {
 
   const message = composeMessage({ paddleboardingMessages, surfingMessages });
 
-  console.log(message);
+  await Promise.all(sendText(message));
+}
 
-  console.log(message.length);
+function sendText(message: string) {
+  return phoneNumbers.map(async (number) => {
+    const params = {
+      PhoneNumber: number,
+      Message: message,
+    };
+
+    await new aws.SNS({ apiVersion: "2010-03-31" }).publish(params).promise();
+  });
 }
